6b568714796f19417bbd09b806ed99e2da50359e
FERROX98 <danielferro19@gmail.com>
2025-03-20 21:37:14 +0100

    wip

--- HW1/project1_ComposingImages/project1.js
+++ HW1/project1_ComposingImages/project1.js
@@ -3,8 +3,6 @@
 // fgOpac is the opacity of the foreground image.
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite(bgImg, fgImg, fgOpac, fgPos) {
-    
-    let isFirst = false;
 
     let fgWidth = fgImg.width;
     let fgHeight = fgImg.height;
@@ -16,26 +14,8 @@ function composite(bgImg, fgImg, fgOpac, fgPos) {
     let bgWidth = bgImg.width;
     let bgHeight = bgImg.height;
 
-    if (isFirst == false){
-        console.log("START");
-        console.log("bgImg: ", bgImg);
-        console.log("fgImg: ", fgImg);
-        console.log("fgOpac: ", fgOpac);
-        console.log("fgPos: ", fgPos);
-        console.log("fgWidth: ", fgWidth);
-        console.log("fgHeight: ", fgHeight);
-        console.log("bgWidth: ", bgWidth);
-        console.log("bgHeight: ", bgHeight);
-    }
-
     if ((fgX< 0 && Math.abs(fgX) >= fgWidth) || (fgY<0 && Math.abs(fgY) >= fgHeight) || fgX >= bgWidth || fgY >= bgHeight) {
          console.log("fgPos is out of bounds");
-        // console.log("fgX: ", fgX);
-        // console.log("fgY: ", fgY);
-        // console.log("fgWidth: ", fgWidth);
-        // console.log("fgHeight: ", fgHeight);
-        // console.log("bgWidth: ", bgWidth);
-        // console.log("bgHeight: ", bgHeight); 
         return;
     }
 
@@ -53,12 +33,6 @@ function composite(bgImg, fgImg, fgOpac, fgPos) {
         blendYpos = fgY * -1;
         fgY = 0;
     }
-    if (isFirst == false){
-        console.log("blendXpos: ", blendXpos);
-        console.log("blendYpos: ", blendYpos);
-        console.log("fgX: ", fgX);
-        console.log("fgY: ", fgY);
-    }
 
     let currFgX = 0;
     let currFgY = 0;
@@ -66,71 +40,40 @@ function composite(bgImg, fgImg, fgOpac, fgPos) {
     let currBgX = fgX;
     let currBgY = fgY;
 
-    console.log("Math.min(fgHeight - blendYpos, bgHeight - currBgY): ", Math.min(fgHeight - blendYpos, bgHeight - currBgY));
-    console.log("Math.min(fgWidth - blendXpos, bgWidth - currBgX): ", Math.min(fgWidth - blendXpos, bgWidth - currBgX));
-    console.log("fgHeight - blendYpos: ", fgHeight - blendYpos);
-    console.log("bgHeight - currBgY: ", bgHeight - currBgY);
-    console.log("fgWidth - blendXpos: ", fgWidth - blendXpos);
-    console.log("bgWidth - currBgX: ", bgWidth - currBgX);
-    console.log("fgHeight - currFgY: ", fgHeight - currFgY);
-    console.log("fgWidth - currFgX: ", fgWidth - currFgX);
     for (currFgY = blendYpos;  currFgY < blendYpos + Math.min(fgHeight - blendYpos, bgHeight - fgY); currFgY++) {
-
+        
         for (currFgX = blendXpos;  currFgX < blendXpos + Math.min(fgWidth - blendXpos, bgWidth - fgX) ; currFgX++) {
 
             fgRed = fgImg.data[currFgY * fgWidth * 4 + currFgX * 4];
             fgGreen = fgImg.data[currFgY * fgWidth * 4 + currFgX * 4 + 1];
             fgBlue = fgImg.data[currFgY * fgWidth * 4 + currFgX * 4 + 2];
             fgAlpha = fgImg.data[currFgY * fgWidth * 4 + currFgX * 4+ 3]/255;
-        
-        
             
-                // Blend with background
-                if ((currBgY * bgWidth * 4 + currBgX * 4 + 3 )< bgImg.data.length) {
-                    
-                    let indexBg = currBgY * bgWidth * 4 + currBgX * 4 ; 
-                    
-                    // Normalized 
-                    bgAlpha =  bgImg.data[indexBg + 3] / 255; 
-                    blendedAlpha = (fgOpac * fgAlpha + bgAlpha * (1 - fgAlpha * fgOpac));
-                    bgImg.data[indexBg + 3] =  blendedAlpha * 255; 
-                    
-                    
-                    // Red 
-                    bgImg.data[indexBg] = ((fgRed * fgAlpha * fgOpac) + (1 - fgAlpha * fgOpac) + bgAlpha *  bgImg.data[indexBg])/ blendedAlpha ; 
-                    // Green
-                    bgImg.data[indexBg + 1] = ((fgGreen * fgAlpha * fgOpac) + (1 - fgAlpha * fgOpac) + bgAlpha *  bgImg.data[indexBg + 1])/ blendedAlpha ; 
-                    // Blue
-                    bgImg.data[indexBg + 2] = ((fgBlue * fgAlpha * fgOpac) + (1 - fgAlpha * fgOpac) + bgAlpha *  bgImg.data[indexBg + 2])/ blendedAlpha ; 
-                    //bgImg.data[indexBg + 2] = (fgOpac * fgAlpha + bgAlpha * bgImg.data[indexBg + 2] * (1 - fgAlpha * fgOpac) + (fgBlue * fgAlpha * fgOpac)) / blendedAlpha;
+            let indexBg = currBgY * bgWidth * 4 + currBgX * 4 ; 
+            // Blend with background
+            if ((indexBg + 3 )< bgImg.data.length) {
                 
-                }
-                currBgX ++;
+                // Normalized 
+                bgAlpha =  bgImg.data[indexBg + 3] / 255; 
+                blendedAlpha = (fgOpac * fgAlpha + bgAlpha * (1 - fgAlpha * fgOpac));
+                bgImg.data[indexBg + 3] =  blendedAlpha * 255; 
+                
+                // Red 
+                bgImg.data[indexBg] = ((fgRed * fgAlpha * fgOpac) + (1 - fgAlpha * fgOpac) + bgAlpha *  bgImg.data[indexBg])/ blendedAlpha ; 
+                // Green
+                bgImg.data[indexBg + 1] = ((fgGreen * fgAlpha * fgOpac) + (1 - fgAlpha * fgOpac) + bgAlpha *  bgImg.data[indexBg + 1])/ blendedAlpha ; 
+                // Blue
+                bgImg.data[indexBg + 2] = ((fgBlue * fgAlpha * fgOpac) + (1 - fgAlpha * fgOpac) + bgAlpha *  bgImg.data[indexBg + 2])/ blendedAlpha ; 
+            
             }
-        
-        
 
-       
-            //  console.log("START BLEND"); 
-              // Blend with background
-              if ((currBgY * bgWidth * 4 + currBgX * 4 + 3) < bgImg.data.length) {
-              
-                currBgX = fgX;
-                } 
-                currBgY ++;
-        
-       
+            currBgX ++;
+            
+        }
+
+        currBgX = fgX;
+        currBgY ++;
+
     }
 
-    console.log("current fgX: ", fgX);
-    console.log("current fgY: ", fgY);
-
-    console.log("current currFgX: ", currFgX);
-    console.log("current currFgY: ", currFgY);
-
-    console.log("current currBgX: ", currBgX);
-    console.log("current currBgY: ", currBgY);
-
-    console.log("fgPos: ", fgPos); 
-    console.log("END");
 }
