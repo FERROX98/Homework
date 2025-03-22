efc01cec763cab8f8603c6de5301c924c279a2de
FERROX98 <danielferro19@gmail.com>
2025-03-22 18:50:35 +0100

    wip HW2

--- HW2/project2_Transformations/project2.js
+++ HW2/project2_Transformations/project2.js
@@ -3,7 +3,35 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+
+	// radians
+	rotation = rotation * Math.PI / 180;
+
+	// 0, 3, 6, 1, 4, 7, 2, 5, 8
+	let transformationMatrix = Array( 1, 0, 0, 0, 1, 0, 0, 0, 1);
+	
+	// Scale 
+	let scaleMatrix = Array( scale, 0 , 0, 0, scale, 0, 0, 0, 1 );
+	
+	// Rotation
+	let rotationMatrix = Array( Math.cos(rotation), Math.sin(rotation), 0, -Math.sin(rotation), Math.cos(rotation), 0, 0, 0, 1); 
+	
+	// Translation 
+	// the last element is 0 for direction vector
+	let translationMatrix = Array(0, 0, 0, 0, 0, 0, positionX, positionY, 0); 
+
+	for (let j = 0; j < transformationMatrix.length/3; j++){
+	for (let i = 0 ; i < transformationMatrix.length ; i+=3){
+
+		//	console.log(" j "+ j, " i ", i, " j+3 ", j+3, " i+1 ", i+1, " j+6 ", j+6, " i+2 ", i+2);
+			//console.log('i + j ', i+j);
+			//console.log('translationMatrix[i+j] ', translationMatrix[i+j]);
+			transformationMatrix[j+i] = (rotationMatrix[j] * scaleMatrix[i] + rotationMatrix[j+3] * scaleMatrix[i+1] + rotationMatrix[j+6] * scaleMatrix[i+2]) + translationMatrix[i+j];
+		}
+	}
+	console.log('transformationMatrix ', transformationMatrix);
+
+	return transformationMatrix;
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -11,5 +39,17 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	let transformationMatrix = Array( 1, 0, 0, 0, 1, 0, 0, 0, 1);
+	console.log('trans1 ', trans1);
+	console.log('trans2 ', trans2);
+	for (let j = 0; j < transformationMatrix.length/3; j++){
+		for (let i = 0 ; i < transformationMatrix.length ; i+=3){
+	
+				//console.log(" j "+ j, " i ", i, " j+3 ", j+3, " i+1 ", i+1, " j+6 ", j+6, " i+2 ", i+2);
+				//console.log('i + j ', i+j);
+				transformationMatrix[j+i] = (trans2[j] * trans1[i] + trans2[j+3] * trans1[i+1] + trans2[j+6] * trans1[i+2]); 
+			}
+		}
+	console.log('transformationMatrix FINALE', transformationMatrix);
+	return transformationMatrix;
 }
