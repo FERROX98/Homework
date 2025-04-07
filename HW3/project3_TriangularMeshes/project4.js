8b2efad93296b3f675c15b230d3087d0945c4786
FERROX98 <danielferro19@gmail.com>
2025-04-07 23:12:00 +0200

    wip  HW3

---
+++ HW3/project3_TriangularMeshes/project4.js
@@ -0,0 +1,144 @@
+// This function takes the projection matrix, the translation, and two rotation angles (in radians) as input arguments.
+// The two rotations are applied around x and y axes.
+// It returns the combined 4x4 transformation matrix as an array in column-major order.
+// The given projection matrix is also a 4x4 matrix stored as an array in column-major order.
+// You can use the MatrixMult function defined in project4.html to multiply two 4x4 matrices in the same format.
+// Ensure that the MatrixMult function is properly defined or imported before using it.
+function GetModelViewProjection( projectionMatrix, translationX, translationY, translationZ, rotationX, rotationY )
+{
+	var trans = [
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		translationX, translationY, translationZ, 1
+	];
+	//trans = BuildIdentityMatrixColumnMajor(4);
+
+	// Rotation x
+	let rotationXMatrix = Array( 1, 0, 0, 0, 
+								0, Math.cos(rotationX), Math.sin(rotationX), 0,
+								0, -Math.sin(rotationX), Math.cos(rotationX), 0,
+								0, 0, 0, 1); 
+	// Rotation Y
+	let rotationYMatrix = Array( Math.cos(rotationY), 0, -Math.sin(rotationY), 0,
+								0, 1, 0, 0,
+								Math.sin(rotationY), 0, Math.cos(rotationY), 0,
+								0, 0, 0, 1); 
+
+
+	//let rotationMatrix= ApplyTransform( rotationYMatrix, rotationXMatrix );
+
+	let rotationMatrix = MatrixMult(rotationYMatrix, rotationXMatrix ); 
+
+	// Removed unused transformationMatrix variable to clean up the	let  transformationMatrix=  BuildIdentityMatrixColumnMajor(4);
+
+	let mvp = BuildIdentityMatrixColumnMajor(4);
+
+	mvp = intermediate.map((value, index) => value + trans[index]);
+
+	// //mvp = MatrixMult( trans,intermediate); 
+	// for (let j = 0; j < transformationMatrix.length/4; j++){
+	// 	for (let i = 0 ; i < transformationMatrix.length ; i+=4){
+ 
+	// 		mvp[j+i] = (intermediate[j] * transformationMatrix[i] + intermediate[j+4] * transformationMatrix[i+1] + intermediate[j+4] * transformationMatrix[i+2] +  intermediate[j+8] * transformationMatrix[i+3]) + trans[i+j];
+	// 	} 
+	// }
+
+
+	return mvp;
+}
+function BuildIdentityMatrixColumnMajor(size) {
+    let identityMatrix = [];
+    for (let col = 0; col < size; col++) {
+        for (let row = 0; row < size; row++) {
+            identityMatrix.push(row === col ? 1 : 0);
+        }
+    }
+    return identityMatrix;
+}
+
+
+function ApplyTransform( trans1, trans2 )
+{
+	let transformationMatrix =  BuildIdentityMatrixColumnMajor(4);
+
+	for (let j = 0; j < transformationMatrix.length/4; j++){
+		for (let i = 0 ; i < transformationMatrix.length ; i+=4){
+	
+				transformationMatrix[j+i] = (trans2[j] * trans1[i] + trans2[j+3] * trans1[i+1] + trans2[j+6] * trans1[i+2] + trans2[j+9] * trans1[i+3]); 
+			}
+		}
+
+	return transformationMatrix;
+} 
+
+
+// [TO-DO] Complete the implementation of the following class.
+
+class MeshDrawer
+{
+	// The constructor is a good place for taking care of the necessary initializations.
+	constructor()
+	{
+		// [TO-DO] initializations
+	}
+	
+	// This method is called every time the user opens an OBJ file.
+	// The arguments of this function is an array of 3D vertex positions
+	// and an array of 2D texture coordinates.
+	// Every item in these arrays is a floating point value, representing one
+	// coordinate of the vertex position or texture coordinate.
+	// Every three consecutive elements in the vertPos array forms one vertex
+	// position and every three consecutive vertex positions form a triangle.
+	// Similarly, every two consecutive elements in the texCoords array
+	// form the texture coordinate of a vertex.
+	// Note that this method can be called multiple times.
+	setMesh( vertPos, texCoords )
+	{
+		// [TO-DO] Update the contents of the vertex buffer objects.
+		this.numTriangles = vertPos.length / 3;
+	}
+	
+	// This method is called when the user changes the state of the
+	// "Swap Y-Z Axes" checkbox. 
+	// The argument is a boolean that indicates if the checkbox is checked.
+	swapYZ( swap )
+	{
+		// [TO-DO] Set the uniform parameter(s) of the vertex shader
+	}
+	
+	// This method is called to draw the triangular mesh.
+	// The argument is the transformation matrix, the same matrix returned
+	// by the GetModelViewProjection function above.
+	draw( trans )
+	{
+		// [TO-DO] Complete the WebGL initializations before drawing
+
+		gl.drawArrays( gl.TRIANGLES, 0, this.numTriangles );
+	}
+	
+	// This method is called to set the texture of the mesh.
+	// The argument is an HTML IMG element containing the texture data.
+	setTexture( img )
+	{
+		// [TO-DO] Bind the texture
+
+		// You can set the texture image data using the following command.
+		gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img );
+
+		// [TO-DO] Now that we have a texture, it might be a good idea to set
+		// some uniform parameter(s) of the fragment shader, so that it uses the texture.
+	}
+	
+	// This method is called when the user changes the state of the
+	// "Show Texture" checkbox. 
+	// The argument is a boolean that indicates if the checkbox is checked.
+	showTexture( show )
+	{
+		// [TO-DO] set the uniform parameter(s) of the fragment shader to specify if it should use the texture.
+	}
+	
+}
+// module.exports = {
+//     BuildIdentityMatrixColumnMajor
+// };
